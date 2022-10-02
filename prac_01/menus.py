e8d5f5e05f3e7adb4ef6314e4e2302d533668867
LHolmes35 <lachlan.holmes1@my.jcu.edu.au>
2022-10-03 00:10:21 +1000

    Creating a menu by using a "while" loop

---
+++ prac_01/menus.py
@@ -0,0 +1,16 @@
+
+MENU = "(H)ello\n(G)oodbye\n(Q)uit"
+
+name = input("Name: ")
+print(MENU)
+option = input(">>> ").upper()
+while option != "Q":
+    if option == "H":
+        print(f"Hello {name}")
+    elif option == "G":
+        print(f"Goodbye {name}")
+    else:
+        print("Invalid choice")
+    print(MENU)
+    option = input(">>> ").upper()
+print("Finished")
