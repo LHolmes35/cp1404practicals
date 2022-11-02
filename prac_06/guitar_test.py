335674d2894de7754632a7f8dd4c7d84002c402a
LHolmes35 <lachlan.holmes1@my.jcu.edu.au>
2022-11-03 00:18:58 +1000

    Update guitar test file

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -13,8 +13,8 @@ def run_test():
     guitar = Guitar(name, year, cost)
     other = Guitar("Another Guitar", 2012, 1512.90)
 
-    print(f"{guitar.name} get_age() - Expected {95}. Got {guitar.get_age()}")
-    print(f"{other.name} get_age() - Expected {5}. Got {other.get_age()}")
+    print(f"{guitar.name} get_age() - Expected {100}. Got {guitar.get_age()}")
+    print(f"{other.name} get_age() - Expected {10}. Got {other.get_age()}")
     print()
     print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
     print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
