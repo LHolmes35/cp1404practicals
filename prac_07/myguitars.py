04dbe132604f7c2cef865534c8da58a52bd73e18
LHolmes35 <lachlan.holmes1@my.jcu.edu.au>
2022-11-09 13:23:19 +1000

    Complete main with adding new objects to lists and exporting updated list to file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -5,19 +5,34 @@ Prac 07
 
 from prac_06.guitar import Guitar
 
+FILENAME = "guitars.csv"
+
 
 def main():
-    """Display guitars using Guitar class"""
+    """Display guitars using Guitar class, add and export new guitars"""
     guitars = get_guitars()
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+    print("Lets add to your collections")
+    new_name = input("Name: ")
+    while new_name != "":
+        new_year = int(input("Year: "))
+        new_cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(new_name, new_year, new_cost)
+        guitars.append(guitar_to_add)
+        print(f"{guitar_to_add} added.")
+        new_name = input("Name: ")
+    with open(FILENAME, "w") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year}, {guitar.cost}", file=out_file)
+    print("Good choices, the collection looks good")
 
 
 def get_guitars():
     """Get guitars from guitars csv file"""
     guitars = []
-    in_file = open("guitars.csv", "r")
+    in_file = open(FILENAME, "r")
     for line in in_file:
         parts = line.strip().split(",")
         parts[1] = int(parts[1])
